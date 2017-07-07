f2fc0af709259e4ad53337d64c1a0b76e627b506
yangyi <yangyirunning@163.com>
2017-07-07 16:05:02 +0800

    2017年07月07日16:04:59

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yangyi on 2017/7/6.
+ */
+
+window.onblur = function() {
+    document.title = "诶,憋走啊!回来啊~喵";//改变title内容
+};
+window.onfocus = function() {
+    document.title = "杨轶的简历";
+};
+
