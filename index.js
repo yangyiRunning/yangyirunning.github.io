ede17e1228acdfc403d0714a19020d0af44c3b52
yangyi <yangyirunning@163.com>
2017-09-08 01:36:28 +0800

    2017年09月08日01:36:27

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /**
  * Created by yangyi on 2017/7/6.
  */
-"use strict";
+'use strict';
+
 window.onblur = function () {
     document.title = "诶,憋走啊!回来啊~喵";//改变title内容
 };
@@ -9,14 +10,14 @@ window.onfocus = function () {
     document.title = "杨轶的简历";
 };
 
-var message = "我是我";
+let message = "我是我";
 
 (function (string) {
     console.log(string);
 })(message);
 
 function alertMe() {
-    let name = document.getElementsByClassName("name")[0].innerHTML;
+    let my_name = document.getElementsByClassName("name")[0].innerHTML;
     let gender = document.getElementsByClassName("gender")[0].innerHTML;
     let age = document.getElementsByClassName("age")[0].innerHTML;
     let work_time = document.getElementsByClassName("work_time")[0].innerHTML;
@@ -25,8 +26,8 @@ function alertMe() {
     let wechat = document.getElementsByClassName("wechat")[0].innerHTML;
     let email = document.getElementsByClassName("email")[0].innerHTML;
     let my_home = document.getElementsByClassName("my_home")[0].innerText;
-    let content =
-        name + "\n" +
+    let my_content =
+        my_name + "\n" +
         gender + "\n" +
         age + "\n" +
         work_time + "\n" +
@@ -35,6 +36,6 @@ function alertMe() {
         wechat + "\n" +
         email + "\n" +
         my_home + "\n";
-    alert(content);
-    window.show.show(content);
+    alert(my_content);
+    window.show.show(my_content);
 }
